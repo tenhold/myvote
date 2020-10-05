16994a39c21889d252e0c0c09a5d62a11235470d
Kris <kris@Lauras-MacBook-Pro-2.local>
2020-10-05 14:35:05 -0500

    [Doc] Add React, Babel, Webpack, Sass

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { join } = require("path");
+const { HotModuleReplacementPlugin } = require("webpack");
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: "./client/index.html",
+  filename: "./index.html",
+});
+
+module.exports = {
+  entry: join(__dirname, "./src/index.jsx"),
+  target: "node",
+  output: {
+    path: join(__dirname, "build"),
+    filename: "app.min.js",
+    publicPath: "/",
+  },
+  plugins: [htmlPlugin],
+  resolve: {
+    modules: [join("node_modules"), "node_modules"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
