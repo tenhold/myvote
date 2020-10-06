b1fb30b070e8db6152a9caacc79e4cc7e01782b3
Kris <kris@Lauras-MacBook-Pro-2.local>
2020-10-05 23:31:17 -0500

    [Feature/Doc] - Add some auth routes. Switch between pages.

--- src/components/style-components/NavBar.jsx
+++ src/components/style-components/NavBar.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React from 'react';
+import Homepage from '../Homepage.jsx';
+import Index from '../../index.jsx';
 
-const NavBar = () => (
-  <div>
-    <ul id="nav">
-      <li>
-        <a href="#">Logout</a>
-      </li>
-      <li>
-        <a href="#">MyProfile</a>
-      </li>
-      <li>
-        <a href="#">MyBallot</a>
-      </li>
-      <li>
-        <a href="#">MyElection</a>
-      </li>
-      <li>
-        <a href="#">MySupport</a>
-      </li>
-    </ul>
+const NavBar = ({ changePage, page }) => (
+  <div className='nav'>
+    {console.log(typeof changePage)}
+    <span
+      className={page === 'myProfile' ? 'nav-selected' : 'nav-unselected'}
+      onClick={() => changePage('myProfile')}
+    >
+      MyProfile
+    </span>
+    <span
+      className={page === 'myBallot' ? 'nav-selected' : 'nav-unselected'}
+      onClick={() => changePage('myBallot')}
+    >
+      MyBallot
+    </span>
+    <span
+      className={page === 'myElection' ? 'nav-selected' : 'nav-unselected'}
+      onClick={() => changePage('myElection')}
+    >
+      MyElection
+    </span>
+    <span
+      className={page === 'mySupport' ? 'nav-selected' : 'nav-unselected'}
+      onClick={() => changePage('mySupport')}
+    >
+      MySupport
+    </span>
+    <span
+      className={page === 'logout' ? 'nav-selected' : 'nav-unselected'}
+      onClick={() => changePage('logout')}
+    >
+      Logout
+    </span>
   </div>
 );
 
