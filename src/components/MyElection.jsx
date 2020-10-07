9fcbde1f9cc77e41cf2eb3138b0b449450618a89
tenhold <tenhold@gmail.com>
2020-10-07 14:26:52 -0500

    [Add] test api data

--- src/components/MyElection.jsx
+++ src/components/MyElection.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavBar from '../components/style-components/NavBar.jsx';
-import Greeting from '../components/style-components/Greeting.jsx';
+// import Greeting from '../components/style-components/Greeting.jsx';
+import { suppressDeprecationWarnings } from 'moment';
 
-const MyElection = () => {
-  return (
-    <div className='container'>
-      <Greeting />
-      <NavBar />
-      <p></p>
-      <h4 className='center'>MyElection</h4>
-      <center>I choose u Pikachu</center>
-    </div>
-  );
-};
+import electionData from '../../data.json';
+
+class MyElection extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      election: electionData
+    }
+  }
+
+  componentDidMount() {
+    const { ballot_item_list } = this.state.election;
+
+    const candidateNames = ballot_item_list.filter(data => {
+      if (data.ballot_item_display_name === 'President of the United States') {
+        return data.candidate_list;
+      }
+    })
+    // .map(pres => pres)
+
+
+    console.log(candidateNames)
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        {/* <Greeting /> */}
+        <NavBar />
+        <p></p>
+        <h4 className='center'>MyElection</h4>
+        <center>I choose u Pikachu</center>
+      </div>
+    );
+  }
+}
 
 export default MyElection;
