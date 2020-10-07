2e93d7e254cd81a369164b662644d8754b5516fd
tenhold <tenhold@gmail.com>
2020-10-07 01:06:24 -0500

    [Add] search bar accessing the database

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 class Friends extends Component {
   constructor(props) {
     super(props);
 
+    this.state ={
+      search: ''
+    };
+
+    this.handleSearch = this.handleSearch.bind(this);
+    this.keyClick = this.keyClick.bind(this);
+  }
+
+  handleSearch(e) {
+
+    this.setState({
+      search: e.target.value
+    });
+  }
+
+  keyClick(e) {
+    if (e.which === 13) {
+      axios.get('/api/users')
+        .then(users => {
+          console.log('users!!!!!', users);
+        })
+    }
   }
 
   render() {
+    const { search } = this.state;
     return(
       <Container>
         <Row>
-          <input type='text' placeholder='find a friend' />
+          <input 
+            type='text' 
+            placeholder='find a friend' 
+            value={search} 
+            onChange={this.handleSearch} 
+            onKeyDown={this.keyClick}
+          />
         </Row>
       </Container>
     );
