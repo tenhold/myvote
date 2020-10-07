2a7d431dd1cb251eb28dc8d766750ee79bde924e
Kris <kris@Lauras-MacBook-Pro-2.local>
2020-10-07 13:22:03 -0500

    [Rename] db/models/Badges.js

--- db/models/badges.js
+++ db/models/badges.js
@@ -14,23 +14,23 @@ const createBadgeSchema = (number, imageURL) => {
   });
 };
 
-const voterBadgeSchema = createBadgeSchema(1, "voterBadge");
-const VoterBadge = model("VoterBadge", voterBadgeSchema);
+const voterBadgeSchema = createBadgeSchema(1, 'voterBadge');
+const VoterBadge = model('VoterBadge', voterBadgeSchema);
 
-const mayorBadgeSchema = createBadgeSchema(5, "mayorBadge");
-const MayorBadge = model("MayorBadge", mayorBadgeSchema);
+const mayorBadgeSchema = createBadgeSchema(5, 'mayorBadge');
+const MayorBadge = model('MayorBadge', mayorBadgeSchema);
 
-const sosBadgeSchema = createBadgeSchema(10, "sosBadge");
-const SosBadge = model("SosBadge", sosBadgeSchema);
+const sosBadgeSchema = createBadgeSchema(10, 'sosBadge');
+const SosBadge = model('SosBadge', sosBadgeSchema);
 
-const gubBadgeSchema = createBadgeSchema(25, "gubBadge");
-const GubBadge = model("GobBadge", gubBadgeSchema);
+const gubBadgeSchema = createBadgeSchema(25, 'gubBadge');
+const GubBadge = model('GobBadge', gubBadgeSchema);
 
-const veepBadgeSchema = createBadgeSchema(50, "veepBadge");
-const VeepBadge = model("VeepBadge", veepBadgeSchema);
+const veepBadgeSchema = createBadgeSchema(50, 'veepBadge');
+const VeepBadge = model('VeepBadge', veepBadgeSchema);
 
-const potusBadgeSchema = createBadgeSchema(100, "potusBadge");
-const PotusBadge = model("PotusBadge", potusBadgeSchema);
+const potusBadgeSchema = createBadgeSchema(100, 'potusBadge');
+const PotusBadge = model('PotusBadge', potusBadgeSchema);
 
 module.exports = {
   VoterBadge,
