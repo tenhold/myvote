66b4e3e9370e539b866ca1c3a4cd68e4dd0927b8
tenhold <tenhold@gmail.com>
2020-10-08 12:40:54 -0500

    [Bug] patch error Postman works

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const ballotRoute = require('../db/routes/Ballot.routes');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(cors());
 
