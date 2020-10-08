85b4795c2f31522b13c7ca84cc46a391eb8344b7
tenhold <tenhold@gmail.com>
2020-10-08 01:06:19 -0500

    [Add] election races page and turn my election to stateless

--- src/components/MyElection.jsx
+++ src/components/MyElection.jsx
@@ -1,74 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import Races from './MyElectionRaces.jsx';
 import NavBar from '../components/style-components/NavBar.jsx';
 import Greeting from '../components/style-components/Greeting.jsx';
 
-import createVoterId from '../../server/helpers/createVoterId';
-import getCandidateList from '../../server/helpers/candidateList';
 
-class MyElection extends Component {
-  constructor(props) {        // should have the current user logged in information
-    super(props);  
+const MyElection = (({ ballotList }) => {
+  return (
+    <div className='container'>
+      <Greeting />
+      <NavBar />
+      <h4 className='center'>MyElection</h4>
+      <Races />
+    </div>
+  );
+});
 
-    this.state = {
-      ballotList: []
-    }
-  
-  }
-
-  componentDidMount() {
-    createVoterId()
-      .then(data => {
-        const { voter_device_id } = data.data;
-        getCandidateList(voter_device_id, '1217 Magazine St nola la')
-          .then(data => {
-            const { ballot_item_list } = data.data;
-            this.setState({
-              ballotList: ballot_item_list
-            });
-            console.log(this.state)
-          });
-      });  
-  }
-  
-  
-  render() {
-    const { ballotList } = this.state;
-    if (!ballotList.length) {
-      return (
-        <div className='container'>
-          <Greeting />
-          <NavBar />
-          <h4 className='center'>MyElection</h4>
-           <div>loading...</div>
-        </div>
-      );
-    } else {
-      return (
-        <div className='container'>
-          <Greeting />
-          <NavBar />
-          <h4 className='center'>MyElection</h4>
-          <ul>
-            {ballotList.reduce((ballotRace, curBallotRace) => {
-                if (curBallotRace.ballot_item_display_name === "U.S. Senate Louisiana") {
-                  ballotRace = curBallotRace.candidate_list;
-                }
-                return ballotRace;
-              }, {})
-              .map(({ballot_item_display_name, candidate_photo_url_medium}) => (
-                <>
-                  <div>{ballot_item_display_name}</div>
-                  <img src={candidate_photo_url_medium} />
-                </>
-              ))}
-          </ul>
-        </div>
-      )
-        
-      
-    }
-  }
-}
 
 export default MyElection;
