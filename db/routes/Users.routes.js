fa4fd76c7dea3a5e99dc174584554e8aafed197c
tenhold <tenhold@gmail.com>
2020-10-08 12:40:54 -0500

    [Bug] patch error Postman works

--- db/routes/Users.routes.js
+++ db/routes/Users.routes.js
@@ -58,8 +58,6 @@ router.post('/add', async (req, res) => {
 
 router.patch('/:id', async (req, res) => {
   const { id } = req.params;
-  console.log(req.params);
-  // const { voter_device_id } = req.body;
   try {
     await Users.findByIdAndUpdate(id, req.body);
     const getUdatedUser = await Users.findOne({ _id: id });
--- src/components/MyProfile/Confirm.jsx
+++ src/components/MyProfile/Confirm.jsx
@@ -21,8 +21,17 @@ export class Confirm extends Component {
         party_affiliation
     } = this.props.values;
     const { _id } = this.props.user;
-    console.log(_id);
-    axios.patch(`/api/users/:${_id}`)
+    axios.patch(`/api/users/${_id}`, {
+      firstName,
+        lastName,
+        email,
+        address,
+        city,
+        state,
+        zipcode,
+        dob,
+        party_affiliation
+    })
       .then((data) => {
         console.log('axios patch', data);
       })
